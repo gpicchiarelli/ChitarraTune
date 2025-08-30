97db011da7cbda43af0c1a34a30b9f4b6d2207a7
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2025-08-30 19:53:48 +0200

    Initial Commit

---
+++ ChitarraTuneTests/ChitarraTuneTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChitarraTuneTests.swift
+//  ChitarraTuneTests
+//
+//  Created by GIACOMO PICCHIARELLI on 30/08/25.
+//
+
+import Testing
+
+struct ChitarraTuneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
